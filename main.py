ac53f4535d268080aaab9ee18671e5e3025da6d7
p3drowski <epaivapessoa@gmail.com>
2025-12-01 15:25:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# --- CORS ---
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# --- Rotas ---
 @app.get("/")
 def home():
     return {"mensagem": "API do Pedro funcionando!"}
